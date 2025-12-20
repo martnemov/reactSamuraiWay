f1c1b8b4c6597ec6da65489ea5f0144abb787a35
Mart Nemov <mr.nemov@gmail.com>
2025-12-20 14:44:27 +0500

    add lesson 7

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,28 +1,57 @@
 import './App.css'
 
-function App() {
-    const tasks = [
-        {id: 0, title: "Купить продукты на неделю", isDone: false},
-        {id: 1, title: "Полить цветы", isDone: true},
-        {id: 2, title: "Сходить на тренировку", isDone: false},
-    ]
-
+export function App() {
     return (
-        <div>
-            <h1>Список дел</h1>
-            <ul>
-                {tasks.map(tasks => {
-                    return (
-                        <li key={tasks.id}>
-                                <div>{tasks.title}</div>
-                                <input type="checkbox" defaultChecked={tasks.isDone}/>
-                        </li>
+        <>
+            <div className="music-player">
+                <h4>1. Классы</h4>
+                <h1 id="title" className="player-title">
+                    Мой музыкальный плеер
+                </h1>
+                <hr/>
 
-                    )
-                })}
-            </ul>
-        </div>
+                <h4>2. maxlength</h4>
+                <input
+                    type="search"
+                    id="search"
+                    placeholder="Поиск музыки"
+                    maxLength={50}
+                    value="Начните печатать..."
+                    disabled={true}
+                />
+                <hr/>
+
+                <h4>3. rows, cols, maxLength, readonly</h4>
+                <textarea
+                    placeholder="Оставьте комментарий к треку..."
+                    rows={3}
+                    cols={40}
+                    maxLength={200}
+                    readOnly
+                    disabled={true}
+                ></textarea>
+                <hr/>
+
+                <h4>4. tabindex</h4>
+                <button tabIndex={0}>Click</button>
+                <hr/>
+
+                <h4>5. colspan, rowspan</h4>
+                <table border={1}>
+                    <tbody>
+                    <tr>
+                        <td colSpan={2}>1</td>
+                        <td rowSpan={3}>2</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <hr/>
+
+                <h4>6. for</h4>
+                <input type="radio" id="repeat-off" name="repeat" disabled={true}/>
+                <label htmlFor="repeat-off">Без повтора</label>
+                <hr/>
+            </div>
+        </>
     )
 }
-
-export default App
