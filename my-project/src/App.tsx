20f5f7b0f1751f232566ad1458e741b78b04faa0
Mart Nemov <mr.nemov@gmail.com>
2025-12-20 20:55:57 +0500

    add task list rendering logic with dynamic styles in App component

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,56 +1,70 @@
 import './App.css'
 
+const tasks =
+    [
+        {
+            id: 1,
+            title: "Купить продукты на неделю",
+            isDone: false,
+            addedAt: "1 сентября",
+            priority: 2,
+        },
+        {
+            id: 2,
+            title: "Полить цветы",
+            isDone: true,
+            addedAt: "2 сентября",
+            priority: 0,
+        },
+        {
+            id: 3,
+            title: "Сходить на тренировку",
+            isDone: true,
+            addedAt: "3 сентября",
+            priority: 1,
+        },
+    ]
+
+const backgroundColor: Record<number, string>  = {
+    0: '#ffffff',
+    1: '#ffd7b5',
+    2: '#ffb38a',
+    3: '#ff9248',
+    4: '#ff6700',
+}
+
 export function App() {
+    if (!tasks) {
+        return <h1>Загрузка...</h1>
+    }
+
+    if (!tasks.length) {
+        return <h1>Задачи отстутствуют</h1>
+    }
+
     return (
         <>
-            <div className="music-player">
-                <h4>1. Классы</h4>
-                <h1 id="title" className="player-title">
-                    Мой музыкальный плеер
-                </h1>
-                <hr/>
-
-                <h4>2. maxlength</h4>
-                <input
-                    type="search"
-                    id="search"
-                    placeholder="Поиск музыки"
-                    maxLength={50}
-                    value="Начните печатать..."
-                    disabled={true}
-                />
-                <hr/>
-
-                <h4>3. rows, cols, maxLength, readonly</h4>
-                <textarea
-                    placeholder="Оставьте комментарий к треку..."
-                    rows={3}
-                    cols={40}
-                    maxLength={200}
-                    readOnly
-                    disabled={true}
-                ></textarea>
-                <hr/>
-
-                <h4>4. tabindex</h4>
-                <button tabIndex={0}>Click</button>
-                <hr/>
-
-                <h4>5. colspan, rowspan</h4>
-                <table border={1}>
-                    <tbody>
-                    <tr>
-                        <td colSpan={2}>1</td>
-                        <td rowSpan={3}>2</td>
-                    </tr>
-                    </tbody>
-                </table>
-                <hr/>
-
-                <h4>6. for</h4>
-                <input type="radio" id="repeat-off" name="repeat" disabled={true}/>
-                <label htmlFor="repeat-off">Без повтора</label>
-                <hr/>
+            <div>
+                <ul style={{display: "flex", flexDirection: 'column', gap: '20px'}}>
+                    {tasks.map(task =>
+                        <li style={{border: '4px solid black', listStyleType: 'none', width: '300px', padding: '20px',
+                        backgroundColor: backgroundColor[task.priority]}}
+                            key={task.id}>
+                            <div>
+                                <span>Заголовок:</span>
+                                <span
+                                    style={{textDecorationLine: task.isDone ? 'line-through' : 'none'}}>{task.title}</span>
+                            </div>
+                            <div>
+                                <span>Статус:</span>
+                                <input type="checkbox" defaultChecked={task.isDone}/>
+                            </div>
+                            <div>
+                                <span>Дата создания задачи:</span>
+                                <span>{task.addedAt}</span>
+                            </div>
+                        </li>)}
+                </ul>
             </div>
         </>
     )
