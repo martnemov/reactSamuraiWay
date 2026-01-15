23621329856e55bd5e93156501126ec3e0f51f97
Mart Nemov <mr.nemov@gmail.com>
2026-01-15 17:25:24 +0500

    feat: add dynamic "Add to basket" button with inCart state and visual feedback

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,9 +1,22 @@
+import {useState} from "react";
+
 export function ProductList (prop) {
+    const [inCart, setInCart] = useState(false)
+
     return <div style={{display: 'flex', flexDirection: 'column', border: '4px solid black', padding: '20px', width: '300px', gap: '20px'}}>
         <div>id: {prop.item.id}</div>
         <div>name: {prop.item.name}</div>
         <div>price: {prop.item.price}</div>
         <div>category: {prop.item.category}</div>
-        <button>Add to basket</button>
+        <button style={{background: inCart ? 'yellow': ''}} onClick={() => {
+            if (inCart) {
+                alert('Товар удален из корзины');
+            } else {
+                alert('Товар добавлен в корзину');
+            }
+            setInCart(prev => !prev);
+        }}>
+            <span>{inCart ? 'товар в корзине' : 'товар удален из корзины'}</span>
+        </button>
         </div>
 }
