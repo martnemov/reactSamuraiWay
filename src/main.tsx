0cadb7c45a85ed8f82162584083b18c287b98cb3
Mart Nemov <mr.nemov@gmail.com>
2026-01-27 17:04:39 +0500

    feat: add `TogglePage` component with reusable hooks (`useToggle`, `useText`) and interactive subcomponents; set `TogglePage` as the app entry point (lesson 23)

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { MainPage } from './ui/MainPage.tsx';
+import { TogglePage } from './ui/TooglePage.tsx';
 
-createRoot(document.getElementById('root')!).render(<MainPage />);
+createRoot(document.getElementById('root')!).render(<TogglePage />);
